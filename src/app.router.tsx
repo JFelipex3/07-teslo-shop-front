e9d073475eb5e9a9813eb8d3600a52c9109a7c4c
JFelipex3 <jfelipe.machuca@gmail.com>
2025-12-20 21:52:15 -0300

    Definir rutas

---
+++ src/app.router.tsx
@@ -0,0 +1,49 @@
+import { createBrowserRouter, Navigate } from "react-router";
+import { ShopLayout } from "./shop/layouts/ShopLayout";
+import { HomePage } from "./shop/pages/home/HomePage";
+import { ProductPage } from "./shop/pages/product/ProductPage";
+import { GenderPage } from "./shop/pages/gender/GenderPage";
+import { AuthLayout } from './auth/layouts/AuthLayout';
+import { LoginPage } from "./auth/pages/login/LoginPage";
+import { RegisterPage } from "./auth/pages/regsiter/RegisterPage";
+import { AdminLayout } from './admin/layouts/AdminLayout';
+import { DashboardPage } from "./admin/pages/dashboard/DashboardPage";
+import { AdminProductPage } from "./admin/pages/product/AdminProductPage";
+import { AdminProductsPage } from "./admin/pages/products/AdminProductsPage";
+
+export const appRouter = createBrowserRouter([
+    // Main routes
+    {
+        path: '/',
+        element: <ShopLayout />,
+        children: [
+            { index: true, element: <HomePage /> },
+            { path: 'product/:idSlug', element: <ProductPage /> },
+            { path: 'gender/:gender', element: <GenderPage /> }
+        ]
+    },
+
+    // Auth Routes
+    {
+        path: '/auth',
+        element: <AuthLayout />,
+        children: [
+            { index: true, element: <Navigate to="/auth/login" /> },
+            { path: 'login', element: <LoginPage /> },
+            { path: 'register', element: <RegisterPage /> },
+        ]
+    },
+
+    // Admin Routes
+    {
+        path: '/admin',
+        element: <AdminLayout />,
+        children: [
+            { index: true, element: <DashboardPage /> },
+            { path: 'product', element: <AdminProductPage /> },
+            { path: 'products/:id', element: <AdminProductsPage /> },
+        ]
+    },
+    { path: '*', element: <Navigate to="/" /> }
+
+])
