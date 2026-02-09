679d3f5f4d421a68969db7118d1e263cd65adc3e
JFelipex3 <jfelipe.machuca@gmail.com>
2026-02-09 13:47:32 -0300

    Característica: Añadir interfaces de Product, ProductsResponse y User, junto con la acción getProducts y el hook useProducts para gestionar productos en la aplicación.

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,13 @@
-import { CustomPagination } from "@/components/custom/CustomPagination"
-import { products } from "@/mocks/products.mock"
-import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
-import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { CustomPagination } from '@/components/custom/CustomPagination';
+import { products } from '@/mocks/products.mock';
+import { CustomJumbotron } from '@/shop/components/CustomJumbotron';
+import { ProductsGrid } from '@/shop/components/ProductsGrid';
+import { useProducts } from '@/shop/hooks/useProducts';
 
 export const HomePage = () => {
+
+    const { data } = useProducts();
+
     return (
         <>
             <CustomJumbotron title="Todos los Productos" />
