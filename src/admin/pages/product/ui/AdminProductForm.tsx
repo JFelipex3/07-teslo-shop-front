697ceb0a0c2b6eb7c27a0c8cf02c5deda164ecce
JFelipex3 <jfelipe.machuca@gmail.com>
2026-02-21 00:49:26 -0300

    Característica: Añadir manejo de archivos en AdminProductForm, incluyendo la visualización de imágenes por cargar.

--- src/admin/pages/product/ui/AdminProductForm.tsx
+++ src/admin/pages/product/ui/AdminProductForm.tsx
@@ -37,6 +37,7 @@ export const AdminProductForm = ({ title, subTitle, product, isPending, onSubmit
   const currentStock = watch('stock');
 
   const labelInputRefTag = useRef<HTMLInputElement>(null);
+  const [files, setFiles] = useState<File[]>([]);
 
   const [dragActive, setDragActive] = useState(false);
 
@@ -83,11 +84,19 @@ export const AdminProductForm = ({ title, subTitle, product, isPending, onSubmit
     setDragActive(false);
     const files = e.dataTransfer.files;
     console.log(files);
+
+    if( !files) return;
+
+    setFiles( (prev) => [...prev, ...Array.from(files)]);
   };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     console.log(files);
+
+    if( !files) return;
+
+    setFiles( (prev) => [...prev, ...Array.from(files)]);
   };
 
   return (
@@ -422,6 +431,25 @@ export const AdminProductForm = ({ title, subTitle, product, isPending, onSubmit
                       </p>
                     </div>
                   ))}
+                </div>
+              </div>
+
+              {/* Imagenes por Cargar */}
+              <div className={ cn("mt-6 space-y-3", { 'hidden': files.length === 0 }) }>
+                <h3 className="text-sm font-medium text-slate-700">
+                  Imágenes por cargar
+                </h3>
+                <div className="grid grid-cols-2 gap-3">
+                  {
+                    files.map( (file, index) => (
+                      <img
+                          src={ URL.createObjectURL(file) }
+                          alt="Product"
+                          key={index}
+                          className="w-full h-full object-cover rounded-lg"
+                        />
+                    ))
+                  }
                 </div>
               </div>
             </div>
